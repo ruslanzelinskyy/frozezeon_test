179c58a90336bf8c3729a3e20fc477d4d5fc6444
Ruslan Zelinskyy <ruslanzelinskyy@gmai.com>
2020-07-11 18:48:59 +0300

    Initial

---
+++ application/models/Login_model.php
@@ -0,0 +1,27 @@
+<?php
+class Login_model extends CI_Model {
+
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    public static function logout()
+    {
+        App::get_ci()->session->unset_userdata('id');
+    }
+
+    public static function start_session(int $user_id)
+    {
+        // если перенедан пользователь
+        if (empty($user_id))
+        {
+            throw new CriticalException('No id provided!');
+        }
+
+        App::get_ci()->session->set_userdata('id', $user_id);
+    }
+
+
+}
